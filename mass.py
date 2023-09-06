4c45e9dc26aace74dbd02fed83a375040509a885
Vijay2678 <vijaykumar.yelwantagi@gmail.com>
2023-09-06 09:22:32 +0530

    this is a first commit

---
+++ mass.py
@@ -0,0 +1,3 @@
+
+[A[Als
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
